47a301e547eb571dd310546c4061369fd5b3f68a
Alexandr Starovoytov <aistarovoytov@yandex.ru>
2024-11-26 21:56:07 +0300

    Configure cpp build

--- shell.nix
+++ shell.nix
@@ -6,14 +6,19 @@ let
       latexmk;
   });
 in
-pkgs.mkShell {
+pkgs.mkShell.override {stdenv = pkgs.llvmPackages_18.stdenv;} {
   buildInputs = [
     pkgs.pandoc
     tex
     pkgs.python312Packages.pygments
-    # pkgs.fira-sans
+    pkgs.cmake
 
     # keep this line if you use bash
     pkgs.bashInteractive
   ];
+
+  nativeBuildInputs = [
+    pkgs.clang-tools_18
+  ];
 }
+
