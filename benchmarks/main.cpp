7c4570b7edc5a99e2f665e581322ded8adfb56f9
Alexandr Starovoytov <aistarovoytov@yandex.ru>
2024-11-27 21:33:43 +0300

    Impl pipe example

--- benchmarks/main.cpp
+++ benchmarks/main.cpp
@@ -1,6 +1,73 @@
+#include <cstring>
 #include <iostream>
+#include <cstdint>
+#include <array>
+
+using std::string_literals::operator""s;
+
+#include <unistd.h>
+#include <wait.h>
+
+constexpr std::size_t kBufSize = 10;
+
+auto LogSyscallError() -> void {
+   auto err = errno;
+   std::cerr << strerrorname_np(err) << ": " << strerror(err) << std::endl;
+}
 
 auto main(int argc, char *argv[]) -> int {
-    std::cout << "hello" << std::endl;
+    std::array<std::int32_t, 2> pipe_file_descriptors;
+    std::array<char, kBufSize> buffer;
+    if (auto err = pipe(pipe_file_descriptors.data()); err == -1) {
+        LogSyscallError();
+        return 1;
+    }
+
+    if (auto child = fork(); child == -1) {
+        LogSyscallError();
+        return 1;
+    } else if (child == 0) {
+        if (close(pipe_file_descriptors[1]) == -1) {
+            LogSyscallError();
+            return 1;
+        }
+
+        while (true) {
+            auto num_read = read(pipe_file_descriptors[0], buffer.data(), kBufSize);
+            if (num_read == -1) {
+                LogSyscallError();
+                return 1;
+            }
+
+            if (num_read == 0) {
+                break;
+            }
+
+            if (write(stdout->_fileno, buffer.data(), num_read) != num_read) {
+                std::cerr << "write failed" << std::endl;
+                return 1;
+            }
+        }
+
+    } else {
+        if (close(pipe_file_descriptors[0]) == -1) {
+            LogSyscallError();
+            return 1;
+        }
+
+        auto s =  "hello world"s;
+        if (write(pipe_file_descriptors[1], s.data(), s.size()) != s.size()) {
+            std::cerr << "write failed" << std::endl;
+            return 1;
+        }
+
+        if (close(pipe_file_descriptors[1]) == -1) {
+            LogSyscallError();
+            return 1;
+        }
+
+        wait(nullptr);
+    }
+
     return 0;
 }
