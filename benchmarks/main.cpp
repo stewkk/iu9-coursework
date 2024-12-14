da035055d3bd4ad205a9dc44e01ba164ac2a7fe8
Alexandr Starovoytov <aistarovoytov@yandex.ru>
2024-12-14 16:32:15 +0300

    Impl pipe benchmark

--- benchmarks/main.cpp
+++ benchmarks/main.cpp
@@ -1,9 +1,43 @@
 #include <benchmark/benchmark.h>
 
-static void BM_SomeFunction(benchmark::State& state) {
+#include <random>
+#include <limits>
+
+#include <stewkk/ipc/fdstreambuf.hpp>
+#include <stewkk/ipc/subprocess.hpp>
+#include <stewkk/ipc/pipe.hpp>
+
+namespace stewkk::ipc {
+
+static void BM_Pipe(benchmark::State& state) {
+  std::size_t count = state.range(0);
+  std::size_t size = state.range(0);
+  std::string message(size, ' ');
+
+  std::random_device rd;
+  static std::mt19937 generator(rd());
+  std::uniform_int_distribution<char> distribution(std::numeric_limits<char>::min(), std::numeric_limits<char>::max());
+  for (auto& el : message) {
+    el = distribution(generator);
+  }
+
+  Subprocess child([&count, &message](FDBufOut out){
+    for (std::size_t i = 0; i < count; ++i) {
+      out.sputn(message.data(), message.size());
+    }
+  }, Pipe());
+
+  std::string got(message.size(), ' ');
+
   for (auto _ : state) {
+    for (std::size_t i = 0; i < count; ++i) {
+      child.stdout.sgetn(got.data(), got.size());
+    }
   }
 }
 
-BENCHMARK(BM_SomeFunction);
+BENCHMARK(BM_Pipe)->RangeMultiplier(2)->Range(8, 8<<10);;
+
+}  // namespace stewkk::ipc
+
 BENCHMARK_MAIN();
