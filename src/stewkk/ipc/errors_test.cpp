5cbadf4153a14b92f8d02e29acd2f5c4559041eb
Alexandr Starovoytov <aistarovoytov@yandex.ru>
2024-12-14 12:52:44 +0300

    Move errors test

---
+++ src/stewkk/ipc/errors_test.cpp
@@ -0,0 +1,20 @@
+#include <gmock/gmock.h>
+
+#include <fcntl.h>
+
+#include <stewkk/ipc/errors.hpp>
+
+using ::testing::Eq;
+using ::testing::ThrowsMessage;
+
+namespace stewkk::ipc {
+
+TEST(ErrorsTest, ThrowsSyscallError) {
+  ASSERT_THAT([] (){
+    if (open("this_file_not_exists", O_RDONLY) == -1) {
+      throw GetSyscallError();
+    }
+  }, ThrowsMessage<SyscallError>(Eq("ENOENT: No such file or directory")));
+}
+
+}  // namespace stewkk::ipc
--- src/stewkk/ipc/pipe_test.cpp
+++ src/stewkk/ipc/pipe_test.cpp
@@ -1,26 +1,13 @@
 #include <gmock/gmock.h>
 
-#include <fcntl.h>
-
 #include <stewkk/ipc/pipe.hpp>
-#include <stewkk/ipc/errors.hpp>
 
 using ::testing::Eq;
-using ::testing::ThrowsMessage;
 
 namespace stewkk::ipc {
 
-TEST(ErrorsTest, ThrowsSyscallError) {
-  ASSERT_THAT([] (){
-    if (open("this_file_not_exists", O_RDONLY) == -1) {
-      throw GetSyscallError();
-    }
-  }, ThrowsMessage<SyscallError>(Eq("ENOENT: No such file or directory")));
-}
-
-TEST(PipeTest, CreatesTwoWrapperClasses) {
+TEST(PipeTest, Simple) {
   Pipe();
-
 }
 
 }  // namespace stewkk::ipc
